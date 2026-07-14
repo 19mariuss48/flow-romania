6ad9b30d05164912630537f36636d7e4baf42ecb
Marius <marius@example.com>
2026-07-14 20:29:21 +0300

    plm

--- vite.config.ts
+++ vite.config.ts
@@ -8,6 +8,9 @@ import { defineConfig } from "@lovable.dev/vite-tanstack-config";
 
 export default defineConfig({
   vite: {
+    preview: {
+      allowedHosts: true
+    },
     ssr: {
       external: ['better-auth', 'zod', 'kysely']
     }
